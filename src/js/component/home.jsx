0b7b43f4752f2f867c8677d3ad0bf317c6ca9989
Jeheiz <carlottajeiz@gmail.com>
2022-09-21 17:08:23 +0000

    cambios todo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+    const [inputValue, setInputValue] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const handlerInput = (e) => {
+        setInputValue(e.target.value)
+    }
+    const handleButton = () => {
+        // tasks.push(inputValue)
+        setTasks([...tasks, inputValue]);
+        setInputValue("");
+    }
+    const handlePress = (e) => {
+        // if (e.keyCode == 13){
+        if (e.key == "Enter") {
+            setTasks([...tasks, inputValue]);
+            setInputValue("");
+        }
+        const Box = () => {
+            const [box, setBox] = useState(0);
+            return <button onClick={() => (Box + 1)}>{box}No quedan tareas</button>
+        }
+    }
+
+
+const resetButton = (index) => {
+    let temp = tasks;
+    tasks.splice(index, 1);
+    setTasks([...temp]);
+}
+const html = tasks.map((e, i) => {
+    return <li className="elementLista" key={i}>{e}
+        <button className="delete" onClick={() => resetButton(i)} value={tasks}>  X  </button>
+
+    </li>
+})
+
+    return (
+        <div className="text-contenedor">
+            <h1>ToDos</h1>
+
+
+            <input className="todo-input1"
+                type="text" onChange={handlerInput} onKeyDown={(e) => handlePress(e)} value={inputValue} />
+
+
+            <ul>
+
+                {html}
+            </ul>
+        </div>
+);
+}
+
 
 export default Home;
